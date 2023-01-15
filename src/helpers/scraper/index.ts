4160e0c442eddab085ee47b37410485422b74058
Nicolas Espinoza <niko.ep@gmail.com>
2023-01-15 22:14:14 +0900

    Save json to file

--- src/helpers/scraper/index.ts
+++ src/helpers/scraper/index.ts
@@ -70,5 +70,15 @@ export const scrapeCollection = async () => {
     });
   }
 
+  // Save JSON to file
+  const jsonContent = JSON.stringify(jsonArray);
+  fs.writeFile("./src/data/db.json", jsonContent, "utf8", function (err) {
+    if (err) {
+      console.log("An error occured while writing JSON Object to File.");
+      return console.log(err);
+    }
+
+    console.log("JSON file has been saved.");
+  });
   return jsonArray;
 };
