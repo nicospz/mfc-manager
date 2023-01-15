47bfc7a6084464ad62cf43acd0d0ae7ba093bbb7
Nicolas Espinoza <niko.ep@gmail.com>
2023-01-15 22:56:49 +0900

    Change chrome to chromium

--- src/helpers/scraper/browser.ts
+++ src/helpers/scraper/browser.ts
@@ -1,14 +1,14 @@
 import puppeteer, { Browser } from "puppeteer-core";
-import chrome from "chrome-aws-lambda";
+import chromium from "chrome-aws-lambda";
 
 export const startBrowser = async () => {
   let browser: Browser;
   try {
     console.log("Opening the browser......");
     browser = await puppeteer.launch({
-      args: chrome.args,
-      executablePath: await chrome.executablePath,
-      headless: chrome.headless,
+      args: chromium.args,
+      executablePath: await chromium.executablePath,
+      headless: chromium.headless,
     });
     return browser;
   } catch (err) {
