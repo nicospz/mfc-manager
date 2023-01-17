6ff5a9253f2eab28229ffe2cf12f6bd1efcf9a3b
Nicolas Espinoza <niko.ep@gmail.com>
2023-01-18 01:25:06 +0900

    Add hourly cron to github actions

--- src/pages/api/scraper.ts
+++ src/pages/api/scraper.ts
@@ -18,38 +18,55 @@ export interface FigureCollectionType {
 }
 
 export default async function handler (
-  _req: NextApiRequest,
-  res: NextApiResponse<FigureCollectionType>
+  req: NextApiRequest,
+  res: NextApiResponse<FigureCollectionType | { message: string }>
 ) {
-  // Get data from your database
-  const figures = await scrapeCollection();
-  const json = {
-    figures,
-    updatedAt: new Date()
-  };
+  if (req.method === 'POST') {
+    try {
+      const { authorization } = req.headers;
 
-  const bucketParams = {
-    Bucket: process.env.R2_BUCKET_NAME as string,
-    Key: 'mfc_collection.json',
-    Body: JSON.stringify(json)
-  };
+      if (authorization === `Bearer ${process.env.API_SECRET_KEY as string}`) {
+        // Get data from your database
+        const figures = await scrapeCollection();
+        const json = {
+          figures,
+          updatedAt: new Date()
+        };
 
-  // Upload the new object to the bucket.
-  console.log('Uploading object to R2...');
-  try {
-    await s3
-      .putObject({
-        Bucket: process.env.R2_BUCKET_NAME as string,
-        Key: 'mfc_collection.json',
-        Body: JSON.stringify(json)
-      })
-      .promise();
-    console.log(
+        const bucketParams = {
+          Bucket: process.env.R2_BUCKET_NAME as string,
+          Key: 'mfc_collection.json',
+          Body: JSON.stringify(json)
+        };
+
+        // Upload the new object to the bucket.
+        console.log('Uploading object to R2...');
+        try {
+          await s3
+            .putObject({
+              Bucket: process.env.R2_BUCKET_NAME as string,
+              Key: 'mfc_collection.json',
+              Body: JSON.stringify(json)
+            })
+            .promise();
+          console.log(
       `Successfully uploaded object: ${bucketParams.Bucket}/${bucketParams.Key}`
-    );
-  } catch (err) {
-    console.log('Error', err);
-  }
+          );
+        } catch (err) {
+          console.log('Error', err);
+        }
 
-  res.status(200).json(json);
+        res.status(200).json(json);
+      } else {
+        res.status(401).json({ message: 'Unauthorized' });
+      }
+    } catch (error) {
+      let message: string = 'An unexpected error has occurred';
+      if (error instanceof Error) message = error.message;
+      res.status(500).json({ message });
+    }
+  } else {
+    res.setHeader('Allow', 'POST');
+    res.status(405).end('Method Not Allowed');
+  }
 }
