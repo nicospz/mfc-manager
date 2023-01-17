5ad70cd8a395744fa89790c16d9786213f8fa387
Nicolas Espinoza <niko.ep@gmail.com>
2023-01-18 01:06:56 +0900

    Handle error when no file found

--- src/pages/api/upcoming-figures.ts
+++ src/pages/api/upcoming-figures.ts
@@ -4,22 +4,26 @@ import { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler (
   _req: NextApiRequest,
-  res: NextApiResponse<FigureCollectionType>
+  res: NextApiResponse<FigureCollectionType | { message: string }>
 ) {
-  const response = await s3
-    .getObject({
-      Bucket: process.env.R2_BUCKET_NAME as string,
-      Key: 'mfc_collection.json'
-    })
-    .promise();
+  try {
+    const response = await s3
+      .getObject({
+        Bucket: process.env.R2_BUCKET_NAME as string,
+        Key: 'mfc_collection.json'
+      })
+      .promise();
 
-  const body = JSON.parse(
-    response.Body?.toString('utf-8') ?? '{}'
-  ) as FigureCollectionType;
-  const data = {
-    updatedAt: body.updatedAt,
-    figures: body.figures.filter((figure) => figure.status === 'Ordered')
-  };
+    const body = JSON.parse(
+      response.Body?.toString('utf-8') ?? '{}'
+    ) as FigureCollectionType;
+    const data = {
+      updatedAt: body.updatedAt,
+      figures: body.figures.filter((figure) => figure.status === 'Ordered')
+    };
 
-  res.status(200).json(data);
+    res.status(200).json(data);
+  } catch {
+    res.status(404).json({ message: 'File found' });
+  }
 }
