5080f55fc4d0bc4851b485599f12f1615f2fc41b
Nicolas Espinoza <niko.ep@gmail.com>
2023-01-18 10:51:27 +0900

    Change scraper signing wait for dom instead of full load

--- src/helpers/scraper/index.ts
+++ src/helpers/scraper/index.ts
@@ -10,12 +10,16 @@ const SIGNIN_URL = 'https://myfigurecollection.net/session/signin/';
 const COLLECTION_URL = 'https://myfigurecollection.net/manager/collection/';
 
 export const scrapeCollection = async () => {
+  let startTime = Date.now();
   const browser = await startBrowser();
   if (!browser) return [];
   const page = await browser.newPage();
+  console.log('Browser opened in', Date.now() - startTime, 'ms');
 
+  startTime = Date.now();
   console.log(`Navigating to ${SIGNIN_URL}...`);
   await page.goto(SIGNIN_URL);
+  console.log('Page loaded in', Date.now() - startTime, 'ms');
 
   // Sign in
   await page.type(
@@ -26,25 +30,33 @@ export const scrapeCollection = async () => {
     'input[type=password][name=password]',
     process.env.MFC_PASSWORD as string
   );
+  console.log('Signing in...');
+  startTime = Date.now();
   await page.click('input[type=submit]');
-  await page.waitForNavigation();
+  await page.waitForNavigation({ waitUntil: 'domcontentloaded' });
+  console.log('Signed in in', Date.now() - startTime, 'ms');
 
   // Go to collection
-  console.log(`Navigating to ${'asdasd'}...`);
+  console.log(`Navigating to ${COLLECTION_URL}...`);
+  startTime = Date.now();
   await page.goto(COLLECTION_URL);
+  console.log('Page loaded in', Date.now() - startTime, 'ms');
 
   // Opening Export CSV Menu
   console.log('Opening Export CSV Menu...');
+  startTime = Date.now();
   const exportCsvButtonList = await page.$x(
     "//a[contains(@class, 'action export')]"
   );
   const exportCsvButton =
     exportCsvButtonList[0] as unknown as ElementHandle<Element>;
   await exportCsvButton.evaluate((b) => { (b as HTMLAnchorElement).click(); });
-  await new Promise((r) => setTimeout(r, 1000));
+  await new Promise((r) => setTimeout(r, 750));
+  console.log('Export CSV Menu opened in', Date.now() - startTime, 'ms');
 
   // Clicking CSV Export
   console.log('Clicking CSV Export...');
+  startTime = Date.now();
   const downloadButtonList = await page.$x("//input[@value='CSV Export']");
   const downloadButton =
     downloadButtonList[0] as unknown as ElementHandle<Element>;
@@ -55,10 +67,12 @@ export const scrapeCollection = async () => {
   });
   await downloadButton.evaluate((b) => { (b as HTMLAnchorElement).click(); });
   await new Promise((r) => setTimeout(r, 500));
+  console.log('File downloaded in', Date.now() - startTime, 'ms');
 
   // Converting CSV to JSON
   let jsonArray = [];
   console.log('Converting CSV to JSON...');
+  startTime = Date.now();
   const dir = fs.readdirSync('/tmp/');
   const csvFile = dir.find((file) => file.includes('.csv'));
   if (csvFile) {
@@ -71,6 +85,7 @@ export const scrapeCollection = async () => {
       if (err) console.error(err);
     });
   }
+  console.log('CSV converted to JSON in', Date.now() - startTime, 'ms');
 
   return (
     jsonArray
